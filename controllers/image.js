92773dac5abc2b8c08ce374c60de36ad88458be5
Your Name <you@example.com>
2023-05-07 17:27:28 +0100

    Back End

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'e544937445aa4380bfd532d9abcb80a6' 
+});
+
+const handleApiCall = (req, res) => {
+	app.models.predict('face-detection', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db)=>{
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0].entries);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'smartbrain',
+    database : 'smart-brain'
+  }
+});
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=> {
+	res.send('SUCCESS')
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) } )
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)} )
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(3001, () => {
+	console.log('app is running')
+})
+
+
+
+
+/*
+
+root route that responds with this is working
+/ --> res = this is working
+
+/signin --> POST  responds w// SUCCESS or FAIL
+
+/register --> POST responds w/ user created
+
+/profile/:userId --> GET = user
+
+/image --> PUT --> updated user 
+
+*/
