029e7201e601f3837b659fd1ab3f8ca91aec668a
Your Name <you@example.com>
2023-05-10 18:35:08 +0100

    updating database details

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
     host : process.env.DATABASE_HOST,
     port : 5432,
     user : process.env.DATABASE_USER,
@@ -43,7 +45,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
 	console.log('app is running')
 })
 
